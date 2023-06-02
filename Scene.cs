03b7f701ee9cde2fcc049b5734c520479e0f00e8
MaxElano <max.ferket2004@gmail.com>
2023-06-02 15:55:29 +0200

    Clean

--- Scene.cs
+++ Scene.cs
@@ -36,12 +36,6 @@ namespace RayTracer
 
             lights.Add(new Light(new Vector3(4, 5, 0), new Vector3(50, 50, 50)));
             lights.Add(new Spotlight(new Vector3(0, -2, 5), new Vector3(20, 20, 20), new Vector3(1, -0.2f, 0), 15f));
-
-            ////-------Test Scene-------
-            //primitives.Add(new TexturedPlane(new Vector3(0,0,-1), new Vector3(0, 0, 4), new Vector3(0.6f, 0.6f, 0.6f), new Vector3(0.8f, 0.8f, 0.8f)));
-            //
-            //lights.Add(new Spotlight(new Vector3(0, 2, 0), new Vector3(50, 50, 50), new Vector3(0, 0.5f, -1), 20f));
-
         }
     }
 }
